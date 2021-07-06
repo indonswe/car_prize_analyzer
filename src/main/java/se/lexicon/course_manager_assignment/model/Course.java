7a731e9c4fd3f5951c38c67ecf21207a6b4fd15a
indonswe <afc2002afc@hotmail.com>
2021-07-06 18:46:18 +0200

    Started on course

--- src/main/java/se/lexicon/course_manager_assignment/model/Course.java
+++ src/main/java/se/lexicon/course_manager_assignment/model/Course.java
@@ -1,5 +1,29 @@
 package se.lexicon.course_manager_assignment.model;
 
+import se.lexicon.course_manager_assignment.data.sequencers.CourseSequencer;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Course {
+
+    private int id;
+    private String courseName;
+    private LocalDate startDate;
+    private int weekDuration;
+    private List<Student> students = new ArrayList<>();
+
+
+
+
+
+    public Course(String courseName, LocalDate startDate, int weekDuration) {
+        this.id = CourseSequencer.nextCourseId();
+        this.courseName = courseName;
+        this.startDate = startDate;
+        this.weekDuration = weekDuration;
+
+    }
 
 }
