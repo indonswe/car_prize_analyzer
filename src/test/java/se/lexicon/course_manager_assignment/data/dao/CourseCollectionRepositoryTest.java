e053bcf2aa6d5bc1d4d5511fc780c43c9cc8590c
indonswe <afc2002afc@hotmail.com>
2021-07-11 11:02:41 +0200

    removecourse

--- src/test/java/se/lexicon/course_manager_assignment/data/dao/CourseCollectionRepositoryTest.java
+++ src/test/java/se/lexicon/course_manager_assignment/data/dao/CourseCollectionRepositoryTest.java
@@ -6,8 +6,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import se.lexicon.course_manager_assignment.data.sequencers.CourseSequencer;
+import se.lexicon.course_manager_assignment.data.sequencers.StudentSequencer;
+import se.lexicon.course_manager_assignment.model.Course;
+import se.lexicon.course_manager_assignment.model.Student;
 
 
+import java.time.LocalDate;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest(classes = {CourseCollectionRepository.class})
@@ -29,5 +36,65 @@ public class CourseCollectionRepositoryTest {
     void tearDown() {
         testObject.clear();
         CourseSequencer.setCourseSequencer(0);
+    }
+
+    @Test
+    void createCourse() {
+        int actual = CourseSequencer.getCourseSequencer();
+        int expected = actual + 1;
+
+        Course course = testObject.createCourse
+                ("Testing", LocalDate.parse("2019-05-05"),1);
+
+        actual = CourseSequencer.getCourseSequencer();
+
+        assertEquals(expected,actual);
+        assertEquals(course.getCourseName(),"Testing");
+        assertEquals(course.getStartDate(),LocalDate.parse("2019-05-05"));
+        assertEquals(course.getWeekDuration(),1);
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByNameContains() {
+    }
+
+    @Test
+    void findByDateBefore() {
+    }
+
+    @Test
+    void findByDateAfter() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findByStudentId() {
+    }
+
+    @Test
+    void removeCourse() {
+        Course unexpected = testObject.createCourse
+                ("Testing", LocalDate.parse("2019-05-05"),1);
+        boolean actual = testObject.removeCourse(unexpected);
+        assertEquals(actual,true);
+    }
+
+    @Test
+    void clear() {
+        Course expected = testObject.createCourse
+                ("Testing", LocalDate.parse("2019-05-05"),1);
+
+        testObject.clear();
+        Collection<Course> actual = testObject.findAll();
+
+
+        assertFalse(actual.contains(expected));
     }
 }
