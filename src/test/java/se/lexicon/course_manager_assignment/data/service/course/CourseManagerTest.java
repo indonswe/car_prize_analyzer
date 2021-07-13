f5ff50645b0f3ca7d918fb8fb6666568e5b90772
indonswe <afc2002afc@hotmail.com>
2021-07-13 11:45:03 +0200

    Starting testing Studentmanager

--- src/test/java/se/lexicon/course_manager_assignment/data/service/course/CourseManagerTest.java
+++ src/test/java/se/lexicon/course_manager_assignment/data/service/course/CourseManagerTest.java
@@ -9,9 +9,17 @@ import se.lexicon.course_manager_assignment.data.dao.CourseCollectionRepository;
 import se.lexicon.course_manager_assignment.data.dao.CourseDao;
 import se.lexicon.course_manager_assignment.data.dao.StudentCollectionRepository;
 import se.lexicon.course_manager_assignment.data.sequencers.CourseSequencer;
+import se.lexicon.course_manager_assignment.data.sequencers.StudentSequencer;
 import se.lexicon.course_manager_assignment.data.service.converter.ModelToDto;
+import se.lexicon.course_manager_assignment.dto.forms.CreateCourseForm;
+import se.lexicon.course_manager_assignment.dto.forms.CreateStudentForm;
+import se.lexicon.course_manager_assignment.dto.views.CourseView;
+import se.lexicon.course_manager_assignment.dto.views.StudentView;
 
 
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(classes = {CourseManager.class, CourseCollectionRepository.class, ModelToDto.class, StudentCollectionRepository.class})
@@ -36,5 +44,65 @@ public class CourseManagerTest {
     void tearDown() {
         courseDao.clear();
         CourseSequencer.setCourseSequencer(0);
+    }
+
+    @Test
+    void create() {
+
+        int actual = CourseSequencer.getCourseSequencer();
+        int expected = actual + 1;
+
+        CreateCourseForm form = new CreateCourseForm
+                (1,"Testjavaprog", LocalDate.parse("2019-05-17"),1);
+
+        CourseView course = testObject.create(form);
+
+        actual = CourseSequencer.getCourseSequencer();
+
+        assertEquals(expected,actual);
+        assertEquals(course.getCourseName(), "Testjavaprog");
+        assertEquals(course.getStartDate(), LocalDate.parse("2019-05-17"));
+        assertEquals(course.getWeekDuration(),1);
+
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void searchByCourseName() {
+    }
+
+    @Test
+    void searchByDateBefore() {
+    }
+
+    @Test
+    void searchByDateAfter() {
+    }
+
+    @Test
+    void addStudentToCourse() {
+    }
+
+    @Test
+    void removeStudentFromCourse() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findByStudentId() {
+    }
+
+    @Test
+    void deleteCourse() {
     }
 }
